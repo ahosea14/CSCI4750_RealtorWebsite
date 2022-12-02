9e65937839f73c3095a304f66cd5d1566b1fe146
willstewartcode <willcstewart@gmail.com>
2022-12-01 19:18:53 -0500

    Added favicon

--- header.php
+++ header.php
@@ -20,4 +20,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"> 
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
