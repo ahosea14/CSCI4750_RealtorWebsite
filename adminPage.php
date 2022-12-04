42d41e952f81dcf2c22a3785dfc1b756dc017ab6
willstewartcode <willcstewart@gmail.com>
2022-12-03 22:03:02 -0500

    Admin page formatting

--- adminPage.php
+++ adminPage.php
@@ -160,11 +160,11 @@
                             <input type="text" class="form-control" id="" placeholder="Enter the realtor's last name">
                         </div>
                         <div class="form-group">
-                            <p><label for="">Brief Bio</label></p>
+                            <label for="">Brief Bio</label>
                             <textarea rows="4" style="width:100%" placeholder="Enter a brief biography"></textarea>
                         </div>
                         <div class="form-group">
-                            <label for="phone">Phone Number</label>
+                            <label for="phone">Phone Number</label><br>
                             <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Enter the realtor's #">
                         </div>
 
